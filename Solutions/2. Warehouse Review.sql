cf9e4ff82fcf148adb5ed3555fcc6f26ca181573
Hieu Nguyen <142287342+Hieu29052005@users.noreply.github.com>
2024-08-17 23:42:18 +0700

    Create 2. Warehouse Review.sql

---
+++ Solutions/2. Warehouse Review.sql
@@ -0,0 +1,12 @@
+SELECT
+	  w.warehouseCode,
+    w.warehouseName,
+    SUM(p.quantityInStock) AS totalInventory
+FROM
+	  mintclassics.warehouses AS w
+LEFT JOIN
+	  mintclassics.products AS p ON w.warehouseCode = p.warehouseCode
+GROUP BY
+	  w.warehouseCode,w.warehouseName
+ORDER BY
+	  totalInventory DESC;
